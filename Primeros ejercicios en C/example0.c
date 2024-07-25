8f3ee2b0041bfcd6bbb3c87717ec35f8eedfffb9
AlejandraAbaunza <alejandraabaunza03@gmail.com>
2024-07-24 22:13:54 -0500

    Create example0.c

---
+++ Primeros ejercicios en C/example0.c
@@ -0,0 +1,38 @@
+/****************************************************************
+Autor: A. Abaunza
+Materia: Sistemas Operativos
+Fecha 18-07-24
+Tema: Introducción al lenguaje C y los entornos
+  -Entornos en la web (CLOUD/NUBE)
+  -Comandos principales de Linux
+  -Programa 0: hola mundo
+******************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  printf("//__//__//__//__//__//__//__//__//__//__//__//__//__//\n");
+  printf("\n");
+  printf("   Hola soy un PROFESIONAL EN FORMACIÓN!!!!!!!!!!!\n");
+  printf("\n");
+  printf("//__//__//__//__//__//__//__//__//__//__//__//__//__//\n");
+  printf("\n");
+  printf("Nombre:  ✰⋆⁺₊⋆ Alejandra Abaunza Suárez ⋆⁺₊⋆✰\n");
+  printf("______________________________________________________\n");
+  printf("\n");
+  printf("                  ✧༺♥༻✧ Materias ✧༺♥༻✧ \n");
+  printf("\n");
+  printf("   ✓ Sistemas Operativos\n");
+  printf("   ✓ Física mecánica\n");
+  printf("   ✓ Calculo Vectorial\n");
+  printf("   ✓ Teoría de la computación\n");
+  printf("   ✓ Estadística descriptiva\n");
+  printf("_____________________________________________________\n");
+  printf("\n");
+  printf("Carrera: ★~~ Ingeniería de sistemas ~~★\n");
+  printf("_____________________________________________________\n");
+
+  
+  return 0;
+  
+}
