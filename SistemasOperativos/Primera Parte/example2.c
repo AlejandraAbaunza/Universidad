c364400bf2bf04db69f3d8b15eb8ae0baadde2cc
AlejandraAbaunza <alejandraabaunza03@gmail.com>
2024-08-15 10:45:55 -0500

    Create example2.c

---
+++ SistemasOperativos/Primera Parte/example2.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  int x;
+  int suma = 0;
+  do{
+    scanf("%d", &x);
+    suma += x;
+  } while(x!=0);
+  printf("La suma es: %d \n", suma);
+  
+  return 0;
+}
