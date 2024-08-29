b985fcf8959623946bd29fa72a014b210e34a25a
AlejandraAbaunza <alejandraabaunza03@gmail.com>
2024-08-29 11:13:31 -0500

    Update labprincipal.c

--- SistemasOperativos/Abaunza_Lab_Mod/labprincipal.c
+++ SistemasOperativos/Abaunza_Lab_Mod/labprincipal.c
@@ -11,78 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CAPACIDAD_INICIAL 4
-
-/*Creación Estructura de Datos para el Vector*/
-typedef struct vectorDinamico{
-        int capacidad;
-        int totalElementos;
-        void **elementos;
-} vectorDinamico;
-
-/*Inicialización de los valores del Vector, casting y reserva de memoria*/
-void vectorInicio(vectorDinamico *V){
-        V->capacidad = CAPACIDAD_INICIAL;
-        V->totalElementos = 0;
-        V->elementos = malloc(sizeof(void *) * V->capacidad); 
-}
-
-/*Retorna el total de elementos*/
-int totalVector(vectorDinamico *V){
-        return V->totalElementos;
-}
-
-/*Redimensiona el vector*/
-static void resizeVector(vectorDinamico *V, int capacidad){
-        printf("Redimensión: %d a %d \n", V->capacidad, capacidad);
-
-        void **elementos = realloc(V->elementos, sizeof(void *) * capacidad);
-        if(elementos){
-                V->elementos = elementos;
-                V->capacidad = capacidad;
-        }
-}
-
-/*Agregar elementos al vector*/
-void addVector(vectorDinamico *V, void *elemento){
-        if(V->capacidad == V->totalElementos)
-                resizeVector(V, V->capacidad*2);
-        V->elementos[V->totalElementos++] = elemento;
-}
-
-/*Liberación de Memoria Dinámica*/
-void freeVector(vectorDinamico *V){
-        free(V->elementos);
-}
-
-/*Obtención de elemto según indice*/
-void *getVector(vectorDinamico *V, int indice){
-        if(indice >= 0 && indice < V->totalElementos)
-                return V->elementos[indice];
-        return NULL;
-}
-
-/*Ubicar un elemento en un indice i*/
-void setVector(vectorDinamico *V, int indice, void *elemento){
-        if(indice >= 0 && indice < V->totalElementos)
-                V->elementos[indice]=elemento;
-}
-
-/*Borrar elemento según indice, redimensión*/
-void borrarVector(vectorDinamico *V, int indice){
-        if(indice < 0 || indice >= V->totalElementos)
-                return; 
-
-        V->elementos[indice] = NULL;
-
-        for(int i=indice; i<V->totalElementos-1; i++){
-                V->elementos[i] = V->elementos[i+1];
-                V->elementos[i+1] = NULL; 
-        }
-        V->totalElementos--;
-        if(V->totalElementos>0 && V->totalElementos == V->capacidad/4)
-                resizeVector(V, V->capacidad/2);
-}
+#include "librerialab.h"
 
 /*Programa principal*/
 int main(){
