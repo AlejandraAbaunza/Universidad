0a16d8b7f7cbf2eb3dc61ed518032a28387d4b65
AlejandraAbaunza <alejandraabaunza03@gmail.com>
2024-09-26 11:38:03 -0500

    Update fork00.c

--- SistemasOperativos/fork/fork00.c
+++ SistemasOperativos/fork/fork00.c
@@ -1,1 +1,21 @@
-a
+/************************************
++ Nombre: Alejandra Abaunza Suarez
++ Fecha: Septiembre 26 / 2024
++ Tema: fork
+**************************************/
+
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]){
+  int x;
+  for(x=0; x < 3; x++){
+    fork();
+    printf("The process ID (PID): %d \n", getpid());
+  }
+  return 0;
+}
